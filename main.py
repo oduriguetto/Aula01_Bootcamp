508a4b97d9a0d5edb2149f04a3b82329f0bf9da2
Odirlei Domingues <oduriguetto@yahoo.com.br>
2025-03-03 14:07:29 -0300

    Inclusão dos Exemplos e Exercicios em aula

---
+++ main.py
@@ -0,0 +1,26 @@
+print("Alguma coisa")
+print(3)
+print ("Olá" + " " + "Turma")
+
+
+print("Olá, " + input("Digite seu nome: ") + "!")
+
+
+# Crie programa que o usuário digita o seu nome e retorna o número de caracteres
+
+print(len(input("Digite seu nome: ")))
+
+# Criar um programa onde o usuário digite dois valores e apareça a soma
+
+print(int(input("Digite o primeiro numero: ")) + int(input("Digite o segundo numero: ")))
+
+
+# Refatore o exercício 02 atribuindo variáveis
+
+# print(len(input("Digite o seu nome: ")))
+
+nome = input("Digite seu nome: ")
+qtde_caracter = len(nome)
+
+print(nome)
+print (qtde_caracter)
